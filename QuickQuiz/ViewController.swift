b839085e47ab3d8bd8b80fc554c591ba08545ebe
Vignesh Purushothaman <vignesh.purushothaman@bestwestern.com>
2021-11-11 20:23:04 -0600

    Initial Commit

---
+++ QuickQuiz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QuickQuiz
+//
+//  Created by Vignesh Vigo on 11/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
